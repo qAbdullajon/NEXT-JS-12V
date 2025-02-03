6a928c0dacf40ae2a347671341e54006ca89600b
qAbduallajon <azikmelor7705@gmail.com>
2025-02-04 00:44:25 +0500

    first commit

---
+++ src/pages/about.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+import React from 'react'
+
+const About = () => {
+  return (
+    <>
+      <Head>
+        <title>About page | Sammi</title>
+      </Head>
+      <div>About page</div>
+    </>
+  )
+}
+
+export default About
---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+import React from 'react'
+
+const Index = () => {
+  return (
+    <>
+      <Head>
+        <title>Index page | Sammi</title>
+      </Head>
+      <div className='text'>Index page</div>
+    </>
+  )
+}
+
+export default Index
